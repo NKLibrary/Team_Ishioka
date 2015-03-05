691adcc5c3fc0e7f5a2ed082ec68dfcc748e6ab8
Andy Feng <andyfengc@gmail.com>
2015-03-04 21:12:00 -0500

    [ADD] reservation model

---
+++ LibrarySystem/Models/Entity/Reservation.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace VirtualCollege.Models.Entity
+{
+    public struct Reservation
+    {
+        string reservationId;
+        string itemType;
+        string itemId;
+        string userId;
+        DateTime expireDate;
+        DateTime processDate;
+        DateTime pickupDate;
+        string statue;
+    }
+}
