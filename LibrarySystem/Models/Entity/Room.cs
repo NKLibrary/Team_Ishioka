5d2dfc8174a7e15b05f0ff90968de81e9f988d44
Andy Feng <andyfengc@gmail.com>
2015-03-07 14:13:26 -0500

    [ADD] room models [ADD] room presenter

---
+++ LibrarySystem/Models/Entity/Room.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace VirtualCollege.Models.Entity
+{
+    public class Room
+    {
+        public string roomId;
+        public int capacity;
+        public string status;
+    }
+}
