8c0362c12d03bcbdbd00a350136ee4eb709d4d0d
maunilshah <maunil99@gmail.com>
2015-04-09 17:08:37 -0500

    Fixed Member_Pay

--- LibrarySystem/Member_Pay.aspx.designer.cs
+++ LibrarySystem/Member_Pay.aspx.designer.cs
@@ -40,15 +40,6 @@ namespace VirtualCollege {
         protected global::System.Web.UI.WebControls.Label FromLbl;
         
         /// <summary>
-        /// startdate control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.HtmlControls.HtmlInputText startdate;
-        
-        /// <summary>
         /// ToLbl control.
         /// </summary>
         /// <remarks>
@@ -56,14 +47,5 @@ namespace VirtualCollege {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Label ToLbl;
-        
-        /// <summary>
-        /// enddate control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.HtmlControls.HtmlInputText enddate;
     }
 }
